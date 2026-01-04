de4b8753e20f0647ae83724f42ddbb77ca9a501b
abbylewis <aslewis@vt.edu>
2026-01-04 00:03:44 -0600

    new data!

---
+++ 01_collect_data/combine_data.R
@@ -0,0 +1,27 @@
+everything <- read_csv("02_extracted_data/Abby_allData.csv", show_col_types = F) %>%
+  mutate(Observer = "Abby Lewis") %>%
+  bind_rows(read_csv("02_extracted_data/Bjorn_allData.csv", show_col_types = F) %>%
+              mutate(Observer = "Bjorn Larson"))  %>%
+  bind_rows(read_csv("02_extracted_data/Eric_allData.csv", show_col_types = F) %>%
+              mutate(Observer = "Eric Larson"))  %>%
+  bind_rows(read_csv("02_extracted_data/Sue_allData.csv", show_col_types = F) %>%
+              mutate(Observer = "Susan Lewis")) 
+
+#Extract data from json files to append
+jsons <- list.files("02_extracted_data", pattern = ".json", recursive = T, full.names = T)
+recents <- jsons %>%
+  map(parse_json_ebird) %>%
+  bind_rows() %>%
+  filter(!`Submission ID` %in% everything$`Submission ID`)
+
+comb <- everything %>%
+  mutate(Time = as.character(Time)) %>%
+  bind_rows(recents) %>%
+  mutate(Date = as.Date(Date, format = "%d %b %Y")) %>%
+  arrange(Date) %>%
+  mutate(check_ID = paste(Date, Time, Latitude, Longitude),
+         `Common Name` = sub(" \\(.+\\)", "", `Common Name`)) %>%
+  filter(!grepl("sp.", `Common Name`),
+         !grepl("/", `Common Name`))
+
+write_csv(comb, "02_extracted_data/for_dashbird.csv")
